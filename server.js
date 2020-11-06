4c482e5a762c8949693139d65421383d0cbda635
Gautham P R <gauthampr18@gmail.com>
2020-11-06 09:31:33 +0530

    Fixed serving issues

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + 'style.css'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res)=> {
     res.sendFile(process.cwd() + '/public/home/home.html');
 })
 
 app.get('/login', (req, res)=> {
-    app.use(express.static(__dirname + '/public/login/login-style.css'));
+    app.use(express.static(__dirname + '/public/login'));
     res.sendFile(process.cwd() + '/public/login/login.html');
 })
 app.listen(process.env.PORT || 3000, ()=> console.log('listening on Port 3000'));
