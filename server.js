19c953aae936f77387b49e474a1e637775c35f29
Gautham P R <gauthampr18@gmail.com>
2020-11-12 13:26:24 +0530

    Changed File Structure

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ require('dotenv').config();
 
 const app = express();
 
-connection();
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res)=> {
-    app.use(express.static(__dirname+'/views/home'));
-    res.sendFile(process.cwd() + '/views/home/home.html');
+    res.sendFile(process.cwd() + '/views/home.html');
 })
 
 app.get('/login', (req, res)=> {
-    app.use(express.static(__dirname + '/public/login'));
-    res.sendFile(process.cwd() + '/views/login/login.html');
+    res.sendFile(process.cwd() + '/views/login.html');
 })
 app.listen(process.env.PORT || 3000, ()=> console.log('listening on Port', process.env.PORT));
