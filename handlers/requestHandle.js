0faa60ce111a07dce547179cd1325764a062ea6b
Gautham P R <gauthampr18@gmail.com>
2020-11-25 21:43:01 +0530

    Final

--- handlers/requestHandle.js
+++ handlers/requestHandle.js
@@ -2,20 +2,39 @@ const customModel = require('./models.js');
 
 module.exports = {
     saveRequest: function saveRequest(requestData, userData){
-        var request = new customModel.Requests(requestData);
-        customModel.Halls.findOne({name: requestData.hallName}, (err, hall)=>{
-            if(err) console.error(err);
-            request.hallID = hall._id;
-        })
-        request.from = new Date(requestData.eventDate + " " + requestData.startTime);
-        request.to = new Date(requestData.eventDate + " " + requestData.endTime);
-        request.requestor = userData._id;
-        request.status = "PENDING";
-        request.next_approver = userData.parentID;
-
-        request.save((err, data)=>{
-            if(err) return console.log(err);
-            console.log(data);
+        return new Promise(async (resolve, reject)=>{
+            var newRequest = new customModel.Requests(requestData);
+            await customModel.Halls.findOne({name: requestData.hallName}, (err, hall)=>{
+                if(err) console.error(err);
+                newRequest.hallID = hall._id;
+                newRequest.from = new Date(requestData.eventDate + " " + requestData.startTime);
+                newRequest.to = new Date(requestData.eventDate + " " + requestData.endTime);
+                newRequest.requestor = userData._id;
+                newRequest.status = "PENDING";
+                newRequest.next_approver = userData.parentID;
+            })
+            await customModel.Requests.find({hallID: newRequest.hallID}, (err, existingRequests)=>{
+                if(err) console.error(err);
+                var available = true;
+                existingRequests.forEach((existingRequest)=>{
+                    if(existingRequest.from <= newRequest.from && newRequest.from < existingRequest.to){
+                        available = false;
+                    }else if(existingRequest.from < newRequest.to && newRequest.to <= existingRequest.to){
+                        available = false;
+                    }else if(newRequest.from <= existingRequest.from && existingRequest.to < newRequest.from){
+                        available = false;
+                    }
+                })
+                if(!available){
+                    reject(new Error("Time Slot Not available"));
+                }else{
+                    newRequest.save((err, data)=>{
+                        if(err) console.error (err);
+                        console.log("SUCCESSFUL");
+                        resolve("Successful");
+                    })
+                }
+            })
         })
     }
 }
