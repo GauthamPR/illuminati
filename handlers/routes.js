f6f2c4f6ab6fdf08b009ee904f0589adb5ea18ed
Gautham P R <gauthampr18@gmail.com>
2020-12-13 15:49:51 +0530

    Fixed

--- handlers/routes.js
+++ handlers/routes.js
@@ -10,7 +10,7 @@ function showError(req, res, err){
 }
 
 function showSuccess(req, res, message){
-    req.flash('succes', message);
+    req.flash('success', message);
     res.redirect('/success');
 }
 
