371cadb690a03f647c8b1aa9a97e300e596d937a
swath-i <swathi.rejitha@gmail.com>
2020-12-17 00:44:07 +0530

    edits

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,12 +1,37 @@
 function myFunction(){  
     document.getElementById("myDropdown").classList.toggle("show");
 }
-
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')){
-    var myDropdown = document.getElementById("myDropdown");
-        if (myDropdown.classList.contains('show')){
-            myDropdown.classList.remove('show');
+window.addEventListener("DOMContentLoaded", (event)=>{
+ 
+    window.onclick = function(e) {
+      if (!e.target.matches('.dropbtn')){
+        var myDropdown = document.getElementById("myDropdown");
+            if (myDropdown.classList.contains('show')){
+                myDropdown.classList.remove('show');
+            }
         }
     }
-}
+    var userName, role;
+
+    document.cookie.split('; ').forEach((elem)=>{
+        if(elem.split('=')[0] == "username"){
+            userName = elem.split('=')[1];
+        }else if(elem.split('=')[0] == "role"){
+            role = elem.split('=')[1];
+        }
+    })
+    var drop = document.getElementById("drop")
+    drop.innerText=userName
+    if(role=="STUDENT") {
+        var addordelete = document.getElementById("add-or-delete")
+        addordelete.remove()
+        
+        var approvelink = document.getElementById("approval-link")
+        approvelink.remove()
+    
+    }
+    else if (role=="TEACHER"){
+        var myreq =document.getElementById("request-link")
+        myreq.remove()
+    }
+})
---
+++ public/js/my-req.js
@@ -0,0 +1,124 @@
+function initialContentLoaded(){
+    return new Promise((resolve)=>{
+        window.addEventListener("DOMContentLoaded", ()=>{
+            resolve("CONTENT LOADED");
+        })
+    })
+}
+
+function getData(url){
+    return new Promise((resolve)=>{
+        fetch(url)
+        .then(response => resolve(response.json()))
+    })
+}
+
+function parseTime(start, end){
+    var start = new Date(start);
+    var end = new Date(end);
+    var date = start.toLocaleDateString();
+    var startTime = start.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    var endTime = end.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    return(date + " (" + startTime + " - " + endTime + ")");
+}
+
+Promise.all([initialContentLoaded(), getData('/getData/user-requests')])
+.then(data=>data[1])
+.then(jsonArray=>{
+    var form = document.getElementById("new")
+    jsonArray.forEach(jsonData=>{
+        var subContainer2=document.createElement("div")
+        subContainer2.setAttribute("class","subContainer2")
+
+       
+
+        var deleteButton=document.createElement("button")
+        deleteButton.setAttribute("type","submit")
+        deleteButton.setAttribute("name",jsonData.id)
+        deleteButton.innerText="Delete"
+        deleteButton.setAttribute("value","delete")
+       
+
+        var desc=document.createElement("div")
+        desc.innerText="Description: "+ jsonData.eventDesc
+        var appby=document.createElement("div")
+        var approverdata
+        if (jsonData.approved_by.length==0)
+             approverdata="None"
+       else
+        {
+            approverdata=jsonData.approved_by.reduce((total,elem)=>{
+                var temp=elem.name+" ("+elem.admNo +")"
+                total.push(temp);
+                return total
+            },[]).join(", ")
+        }
+        appby.innerText="Approved By: " + approverdata
+
+        var subContainer1=document.createElement("div")
+        subContainer1.setAttribute("class","subContainer1")
+
+        var panel=document.createElement("div")
+        panel.setAttribute("class","panel")
+
+        var hallName=document.createElement("div")
+        hallName.innerText=jsonData.hallName
+        var dateAndTime=document.createElement("div")
+        dateAndTime.innerText=parseTime(jsonData.startTime, jsonData.endTime);
+        var eventName=document.createElement("div")
+        eventName.innerText=jsonData.eventName
+        var status=document.createElement("div")
+        status.innerText=jsonData.status
+        if (jsonData.status=="PENDING")
+            status.setAttribute("class","status pending")
+        else if (jsonData.status=="APPROVED")
+            status.setAttribute("class","status approved")
+        else if (jsonData.status=="DENIED")
+            status.setAttribute("class","status denied")
+
+        var container1=document.createElement("div")
+        container1.setAttribute("class","container1")
+        
+        var button=document.createElement("button") 
+        button.setAttribute("type","button")
+        button.setAttribute("class","accordion")
+      
+        var section=document.createElement("div")
+        section.setAttribute("class","section")
+
+       
+        subContainer2.appendChild(deleteButton)
+        subContainer1.appendChild(desc)
+        subContainer1.appendChild(appby)
+        panel.appendChild(subContainer1)
+        panel.appendChild(subContainer2)
+        container1.appendChild(hallName)
+        container1.appendChild(dateAndTime)
+        container1.appendChild(eventName)
+        container1.appendChild(status)
+        button.appendChild(container1)
+        section.appendChild(button)
+        section.appendChild(panel)
+        form.appendChild(section)
+    })
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "grid") {
+          this.parentNode.style.border = "1px solid #ccc";
+          panel.style.display = "none";
+        } else {
+          this.parentNode.style.border = "1px solid #ccc";
+          panel.style.display = "grid";
+        }
+      });
+    }
+})
+.catch((err)=>{
+    console.log(err);
+})
