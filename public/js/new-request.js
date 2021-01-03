a8f44ccc00ad85a405109f8a6f3b89f429081b25
Anaith <aanaith@gmail.com>
2021-01-03 15:05:47 +0530

    newreq

--- public/js/new-request.js
+++ public/js/new-request.js
@@ -31,3 +31,90 @@ Promise.all([initialContentLoaded(), getData('/getData/halls')])
 .catch((err)=>{
     console.log(err);
 })
+function showPopup(info){
+    var timeOut;
+    var popupHolder = document.getElementById("popup-holder");
+    var popup = document.getElementById("popup");
+    if(info.success){
+        popup.innerText = info.message;
+        popup.classList.add("success");
+        timeOut = 1000;
+    }else{
+        popup.innerText = info.error;
+        popup.classList.add("fail");
+        timeOut = 3000;
+    }
+    popup.classList.add("fadeIn");
+    popupHolder.style.display = "grid";
+    setTimeout(()=>{
+        popupHolder.style.display = "none";
+        popup.classList.remove("fail");
+        if(info.url)
+            window.location.replace(info.url)
+    }, timeOut);
+}
+
+function toggleBlock(){
+    var submitButton = document.getElementById("signin-button");
+    submitButton.classList.toggle("loading");
+    if(!submitButton.disabled)
+        submitButton.innerText = "Signing In...";
+    else
+        submitButton.innerText = "Sign In";
+    submitButton.disabled = !submitButton.disabled;
+}
+
+function postRequest(reqInfo){
+    return new Promise((resolve, reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', reqInfo.url);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = function(){
+            if(xhr.status === 200){
+                var response = JSON.parse(xhr.response);
+                resolve({success: true, message: response.message, url: response.redirectLink})
+            }else if(xhr.status === 401){
+                var response = JSON.parse(xhr.response);
+                resolve({success: false, error: response.error})
+            }else{
+                reject(xhr.response);
+            }
+        }
+        var body = (reqInfo.body.map(e=>e.join("=")).join("&"));
+        xhr.send(body);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    var newreqForm = document.getElementById("newreq-form");
+    newreqForm.addEventListener("submit", event=>{
+        event.preventDefault();
+        toggleBlock();
+        var date         = document.getElementById("date").value;
+        var startTime    = document.getElementById("startTime").value;
+        var endTime    = document.getElementById("endTime").value;
+        var hallNo       = document.getElementById("hallno").value;
+        var eventName    = document.getElementById("eventname").value;
+        var eventDesc    = document.getElementById("eventdesc").value;
+        var reqInfo     = {
+            url: "/new-request",
+            body: [
+                ["eventDate", date],
+                ["startTime", startTime],
+                ["endTime", endTime],
+                ["hallNumber", hallNo],
+                ["eventName", eventName],
+                ["eventDesc", eventDesc]
+            ]
+        } 
+        postRequest(reqInfo)
+        .then(response=>{
+            if(!response.success)
+                toggleBlock();
+            showPopup(response);
+        })
+        .catch((response)=>{
+            showPopup({success: false, error: "Unrecognised Response"})
+        })
+    })
+})
